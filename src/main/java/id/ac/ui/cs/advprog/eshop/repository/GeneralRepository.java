9280112b4ef05bdd7743d5cb5a474717e03e30c5
bangjai123 <znaufalilmi@gmail.com>
2024-02-24 18:55:48 +0700

    selesai menerapkan SOLID

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/GeneralRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/GeneralRepository.java
@@ -1,13 +1,14 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
-import id.ac.ui.cs.advprog.eshop.model.Car;
 import id.ac.ui.cs.advprog.eshop.model.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 abstract class GeneralRepository<T extends Product>{
     List<T> itemData = new ArrayList<>();
 
@@ -32,6 +33,9 @@ abstract class GeneralRepository<T extends Product>{
     }
     public void delete(String id){
         itemData.removeIf(item -> item.getItemId().equals(id));
+    }
+    public List<T> getItemData(){
+        return itemData;
     }
 
 }
