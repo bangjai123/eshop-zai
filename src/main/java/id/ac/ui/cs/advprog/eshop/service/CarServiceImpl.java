fe7780f4215d1aac70f3a10accc733412402ceb6
bangjai123 <znaufalilmi@gmail.com>
2024-03-06 00:55:35 +0700

    commit sebelum nugas lagi

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.model.Car;
-import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
