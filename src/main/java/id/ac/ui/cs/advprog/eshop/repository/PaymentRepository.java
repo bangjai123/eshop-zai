24f4371f577ab8c4d2702f978f7e4fa535641d90
bangjai123 <znaufalilmi@gmail.com>
2024-03-06 20:56:32 +0700

    [GREEN] implement PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 
 import java.util.ArrayList;
@@ -8,9 +9,28 @@ import java.util.List;
 public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
 
-    public Payment save(Payment payment){return null;}
-    public Payment findById(String id){return null;}
+    public Payment save(Payment payment){
+        int i = 0;
+        for(Payment savePayment : paymentData) {
+            if(savePayment.getId().equals(payment.getId())){
+                paymentData.remove(i);
+                paymentData.add(i,payment);
+                return payment;
+            }
+            i += 1;
+        }
+        paymentData.add(payment);
+        return payment;
+    }
+    public Payment findById(String id){
+        for(Payment savedPayment : paymentData){
+            if(savedPayment.getId().equals(id)){
+                return savedPayment;
+            }
+        }
+        return null;
+    }
 
-    public ArrayList<Payment> getAllPayments(){return null;}
+    public List<Payment> getAllPayments(){return paymentData;}
 
 }
