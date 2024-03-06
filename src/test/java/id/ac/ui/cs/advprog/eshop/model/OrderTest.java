69fbbf8276dc2a1202ccf1c4783b1a2b30948114
bangjai123 <znaufalilmi@gmail.com>
2024-03-06 08:15:32 +0700

    [RED] Add Order model skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -70,7 +70,7 @@ public class OrderTest {
         Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
                 this.products, 1708560000L,"Safira Sudrajat");
         order.setStatus("CANCELLED");
-        assertEquals("CANCELLED",order.getStatus);
+        assertEquals("CANCELLED",order.getStatus());
     }
 
     @Test
