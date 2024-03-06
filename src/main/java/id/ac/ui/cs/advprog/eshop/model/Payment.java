b1b32323e7aedb73e95b5fbcf6f70a74312dade3
bangjai123 <znaufalilmi@gmail.com>
2024-03-06 20:10:25 +0700

    [REFACTOR] Apply enums to Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,4 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.model;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +13,8 @@ public class Payment {
     String id;
     String method;
     Map<String,String> map;
+
+    //diasumsikan hanya ada dua kemungkina, SUCCESS atau REJECTED
     String status;
 
     //diasumsikan setiap payment adalah pasti terhubung dengan sebuah order
@@ -20,48 +24,26 @@ public class Payment {
         this.id = id;
         this.map = map;
 
-        if(order.equals(null)||!methodIsValid(method)){
+        if(order.equals(null)||!PaymentMethod.contains(method)){
             throw new IllegalArgumentException();
         }
         else{
             this.order = order;
             this.method = method;
         }
-
     }
 
     public Payment(String id, String method, Map<String,String> map, Order order, String status){
        this(id,method,map,order);
-
-       if(statusIsValid(status)){
-           this.status = status;
-       }
-       else{
-           throw new IllegalArgumentException();
-       }
+       this.setStatus(status);
     }
-
-    public boolean methodIsValid(String method){
-        //asumsi hanya dua pilihan karena NPM saya ganjil sehingga hanya perlu voucher dan COD
-        if (method.equals("CashOnDelivery")||method.equals("Voucher")){
-            return true;
-        }
-        return false;
-    }
-
-    public boolean statusIsValid(String status){
-        if(status.equals("SUCCESS")||status.equals("REJECTED")){
-            return true;
-        }
-        return false;
-    }
-
     public void setStatus(String status){
-        if(!statusIsValid(status)){
-            throw new IllegalArgumentException();
+        if(PaymentStatus.contains(status)){
+            this.status = status;
+
         }
         else{
-            this.status = status;
+            throw new IllegalArgumentException();
         }
     }
 }
